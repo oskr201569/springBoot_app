1e64b41742676b23b0217edcecd7d0461c8dfd3f
oskr201569 <oskr201569@gmail.com>
2024-02-20 07:48:20 -0500

    First commit

---
+++ src/test/java/com/clientes/app/demo/ClientesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientes.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
